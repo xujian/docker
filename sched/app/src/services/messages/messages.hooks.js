36eb3da7a7d32091b5a907b53fbcf4629d791b8e
xujian <782135@qq.com>
2018-09-21 00:35:43 +0800

    + sched

---
+++ sched/app/src/services/messages/messages.hooks.js
@@ -0,0 +1,35 @@
+
+
+const processMessage = require('../../hooks/process-message');
+
+module.exports = {
+  before: {
+    all: [],
+    find: [],
+    get: [],
+    create: [processMessage()],
+    update: [],
+    patch: [],
+    remove: []
+  },
+
+  after: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  },
+
+  error: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  }
+};
